400adda79ee8ddcd347e1f0b3b288c1ee415b64e
daniel-silva <jdsc17@gmail.com>
2023-08-14 17:38:28 -0700

    restore

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,6 +9,9 @@ const Footer = ({ metaData, en = false }) => (
               <h4 className="text-black mb-2 mb-md-4">{metaData.title}</h4>
               <p className="text-black">{en ? "Baja Cargo Solutions helps reduce distribution cost by providing dynamic and effective warehousing." : "Logística a tiempo y sin problemas para tus cargas, fletes o movimientos de Exportación e Importación en México."}</p>
               <a href="/condiciones-estandares-de-comercio.pdf" target="_blank">Condiciones estándares de comercio <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" style={{width: 15, height: 15}}><path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" /></svg></a>
+              <h6 className="text-black mt-4 mb-2">{en ? "Hiring" : "Empleos"}</h6>
+              <a href="/vacantes.pdf" target="_blank">Vacante Coordinador de Logistica (Ensenada)<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" style={{width: 15, height: 15}}><path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" /></svg></a>
+
           </div>
           <div className="col-sm-4 col-lg-2 mb-4 mb-md-0">
             <h6 className="text-dark pb-2 pb-md-4">{en ? "Address" : "Dirección"}</h6>
